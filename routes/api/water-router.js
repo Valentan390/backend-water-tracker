fc0c3081180319fcb19f539d128040bc7358beaf
elias-291356 <ikucher.002@gmail.com>
2024-01-12 13:55:14 +0400

    water-routes

---
+++ routes/api/water-router.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { validateBody, isEmptyBody } from "../../middlewares/validateBody.js";
+import watersController from "../../controllers/water-controller.js";
+import authenticate from "../../middlewares/authenticate.js";
+import isValidId from "../../middlewares/isValidId.js";
+import { waterAddSchema, waterUpdateSchema } from "../../models/waters.js";
+
+export const watersRouter = express.Router();
+
+watersRouter.use(authenticate);
+
+watersRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(waterAddSchema),
+  watersController.addWater
+);
+
+watersRouter.patch(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  validateBody(waterUpdateSchema),
+  watersController.updateWaterById
+);
+
+watersRouter.delete("/:id", isValidId, watersController.deleteWaterById);
+
+// -----------------------------------------
+watersRouter.get("/", watersController.getAll);
+export default watersRouter;
