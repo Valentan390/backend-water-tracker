1a1d204eb515bbb27f9efe324709a29664bdf39b
WithinKael <138517664+WithinKael@users.noreply.github.com>
2024-01-14 01:47:41 +0100

    add swagger

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import authRouter from "./routes/api/auth-router.js";
 import userRouter from "./routes/api/user-router.js";
 import watersRouter from "./routes/api/water-router.js";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./swagger.json" assert { type: "json" };
+
 export const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/waters", watersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
