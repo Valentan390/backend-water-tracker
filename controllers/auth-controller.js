c27aa7696c869b481093c9f497a030ab29cd4a4c
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2024-01-27 21:18:23 +0200

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -51,7 +51,7 @@ const signin = async (req, res) => {
     id,
   };
 
-  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "0.1h" });
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
 
   await User.findByIdAndUpdate(id, { token });
 
